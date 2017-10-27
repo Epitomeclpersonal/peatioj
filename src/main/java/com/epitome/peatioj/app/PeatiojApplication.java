4f432ef0d0f42e5fefefb69629c61b5c9bc56f32
genemskim <ms.e.kim@gmail.com>
2017-10-27 12:22:50 +0900

    start

---
+++ src/main/java/com/epitome/peatioj/app/PeatiojApplication.java
@@ -0,0 +1,12 @@
+package com.epitome.peatioj.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeatiojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeatiojApplication.class, args);
+	}
+}
