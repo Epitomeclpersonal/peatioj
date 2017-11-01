768f2646702ab0c25e19161fb5370680c87d975f
genemskim <ms.e.kim@gmail.com>
2017-11-01 15:55:50 +0900

    add RabbitConfig

--- worker-amqp/src/main/java/sample/amqp/SampleAmqpSimpleApplication.java
+++ worker-amqp/src/main/java/sample/amqp/SampleAmqpSimpleApplication.java
@@ -22,33 +22,40 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {
+        "sample.config"
+})
 @RabbitListener(queues = "foo")
 @EnableScheduling
 public class SampleAmqpSimpleApplication {
 
-	@Bean
-	public Sender mySender() {
-		return new Sender();
-	}
+    @Bean
+    public Sender mySender() {
+        return new Sender();
+    }
 
-	@Bean
-	public Queue fooQueue() {
-		return new Queue("foo");
-	}
+    @Bean
+    public Queue fooQueue() {
+        return new Queue("foo");
+    }
 
-	@RabbitHandler
-	public void process(@Payload String foo) {
-		System.out.println(new Date() + ": " + foo);
-	}
+    @RabbitHandler
+    public void process(@Payload String foo) {
+        System.out.println(new Date() + ": " + foo);
+    }
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SampleAmqpSimpleApplication.class, args);
-	}
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleAmqpSimpleApplication.class, args);
+    }
 
 }
---
+++ worker-amqp/src/main/java/sample/config/RabbitConfig.java
@@ -0,0 +1,43 @@
+package sample.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Autowired
+    private ConnectionFactory rabbitConnectionFactory;
+
+    @Bean
+    DirectExchange rubeExchange() {
+        return new DirectExchange("rmq.rube.exchange", true, false);
+    }
+
+    @Bean
+    public Queue rubeQueue() {
+        return new Queue("rmq.rube.queue", true);
+    }
+
+    @Bean
+    Binding rubeExchangeBinding(DirectExchange rubeExchange, Queue rubeQueue) {
+        return BindingBuilder.bind(rubeQueue).to(rubeExchange).with("rube.key");
+    }
+
+    @Bean
+    public RabbitTemplate rubeExchangeTemplate() {
+        RabbitTemplate r = new RabbitTemplate(rabbitConnectionFactory);
+        r.setExchange("rmq.rube.exchange");
+        r.setRoutingKey("rube.key");
+        r.setConnectionFactory(rabbitConnectionFactory);
+        return r;
+    }
+}
