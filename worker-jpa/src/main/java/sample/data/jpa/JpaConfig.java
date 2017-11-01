73d8a45cc7046a2d007b428a4c799669f44e4b7a
jdlee726 <jdlee726@gmail.com>
2017-11-01 22:36:56 +0900

    fix jpa

---
+++ worker-jpa/src/main/java/sample/data/jpa/JpaConfig.java
@@ -0,0 +1,31 @@
+package sample.data.jpa;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+/**
+ * http://www.baeldung.com/the-persistence-layer-with-spring-and-jpa
+ */
+@Configuration
+@ComponentScan(basePackages = {
+        "sample.data.jpa"
+})
+public class JpaConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://epitome.iptime.org:3306/myDb?createDatabaseIfNotExist=true");
+        dataSource.setUsername("mysqluser");
+        dataSource.setPassword("mysqlpass");
+
+        return dataSource;
+    }
+
+}
