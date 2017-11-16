83e7f05251a43280667f8e0de630671309c668dc
genemskim <ms.e.kim@gmail.com>
2017-11-16 17:06:24 +0900

    Tour of Heros chapter5

--- worker-angular/src/app/app.module.ts
+++ worker-angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import {FormsModule} from "@angular/forms";
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
