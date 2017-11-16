8ecbfac8a003fff8340a5e7b439841e7327527c5
genemskim <ms.e.kim@gmail.com>
2017-11-16 16:00:30 +0900

    Tour of Heros chapter3 : add heroes component

--- worker-angular/src/app/app.module.ts
+++ worker-angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
