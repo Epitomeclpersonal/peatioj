c022f21d929700409044e18c7c3a5847d6f2422c
genemskim <ms.e.kim@gmail.com>
2017-11-16 16:32:49 +0900

    Tour of Heros chapter3

--- worker-angular/src/app/app.module.ts
+++ worker-angular/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HeroesComponent } from './heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
