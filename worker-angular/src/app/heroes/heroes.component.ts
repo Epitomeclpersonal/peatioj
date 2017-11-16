799516951a0a27878de4c7b1643d3942f46ea98a
genemskim <ms.e.kim@gmail.com>
2017-11-16 16:56:41 +0900

    Tour of Heros chapter4

--- worker-angular/src/app/heroes/heroes.component.ts
+++ worker-angular/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from "../hero";
+import {HEROES} from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
@@ -17,6 +18,14 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
+
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
 }
 
