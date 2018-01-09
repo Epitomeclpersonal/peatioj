952805238c64883790c7c7b4c08f70fdfc37a47e
jdlee726 <jdlee726@gmail.com>
2018-01-09 11:15:09 +0900

    hibernate

--- peatio-11-database/src/main/java/org/peatio/db/jdbc/mysql/MysqlWrapper.java
+++ peatio-11-database/src/main/java/org/peatio/db/jdbc/mysql/MysqlWrapper.java
@@ -15,33 +15,40 @@ import static com.wix.mysql.distribution.Version.v5_7_latest;
 
 public class MysqlWrapper {
 
-    public static void main(String[] aa) {
+    public static void main(String[] aa) throws InterruptedException {
         HomeConfigurator.changeLogConfiguration();
         MysqlWrapper mysqlWrapper = new MysqlWrapper();
         mysqlWrapper.start();
+        Thread.sleep(3600 * 1000);
+        mysqlWrapper.stop();
     }
+
+    private EmbeddedMysql mysqld;
 
     public void start() {
         MysqldConfig config = aMysqldConfig(v5_7_latest)
                 .withCharset(UTF8)
-                .withPort(2215)
+//                .withPort(2215)
                 .withUser("differentUser", "anotherPassword")
-                .withTimeZone("Europe/Vilnius")
+//                .withTimeZone("Europe/Vilnius")
                 .withTimeout(2, TimeUnit.MINUTES)
                 .withServerVariable("max_connect_errors", 666)
+                .withServerVariable("log_syslog", 0)
                 .build();
 
         DownloadConfig downloadConfig = aDownloadConfig()
                 .withCacheDir(HomeConfigurator.getTmpDir())
                 .build();
 
-        EmbeddedMysql mysqld = anEmbeddedMysql(config, downloadConfig)
+        mysqld = anEmbeddedMysql(config, downloadConfig)
 //                .addSchema("aschema", ScriptResolver.classPathScript("db/001_init.sql"))
 //                .addSchema("aschema2", ScriptResolver.classPathScripts("db/*.sql"))
                 .start();
 
 //do work
+    }
 
+    public void stop() {
         mysqld.stop(); //optional, as there is a shutdown hook
     }
 }
