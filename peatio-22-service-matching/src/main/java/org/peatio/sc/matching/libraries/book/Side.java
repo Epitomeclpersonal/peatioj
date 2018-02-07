81baa68d207f829de21354565947500f3f84a547
jdlee726 <jdlee726@gmail.com>
2018-02-07 14:33:02 +0900

    org.peatio.sc.matching.libraries.book

---
+++ peatio-22-service-matching/src/main/java/org/peatio/sc/matching/libraries/book/Side.java
@@ -0,0 +1,9 @@
+package org.peatio.sc.matching.libraries.book;
+
+/**
+ * The order side.
+ */
+public enum Side {
+    BUY,
+    SELL
+}
---
+++ peatio-22-service-matching/src/main/java/org/peatio/sc/matching/libraries/book/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Parity Order Book implements high-performance order book reconstruction.
+ */
+package org.peatio.sc.matching.libraries.book;
